3582d58845eb384ed9f3470b2ffcb9655c1f1001
Rohit Subramanian <rohitsub11@gmail.com>
2017-06-24 13:38:10 -0700

    fixed bug
    Missing close }

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -80,36 +80,37 @@ class FG_eval {
     for (int i = 0; i < N - 1; i++) {
 
       for (int t = 1; t < N; t++) {
-      AD<double> x1 = vars[x_start + t];
-      AD<double> y1 = vars[y_start + t];
-      AD<double> psi1 = vars[psi_start + t];
-      AD<double> v1 = vars[v_start + t];
-      AD<double> cte1 = vars[cte_start + t];
-      AD<double> epsi1 = vars[epsi_start + t];
+        AD<double> x1 = vars[x_start + t];
+        AD<double> y1 = vars[y_start + t];
+        AD<double> psi1 = vars[psi_start + t];
+        AD<double> v1 = vars[v_start + t];
+        AD<double> cte1 = vars[cte_start + t];
+        AD<double> epsi1 = vars[epsi_start + t];
 
-      AD<double> x0 = vars[x_start + t - 1];
-      AD<double> y0 = vars[y_start + t - 1];
-      AD<double> psi0 = vars[psi_start + t - 1];
-      AD<double> v0 = vars[v_start + t - 1];
-      AD<double> cte0 = vars[cte_start + t - 1];
-      AD<double> epsi0 = vars[epsi_start + t - 1];
+        AD<double> x0 = vars[x_start + t - 1];
+        AD<double> y0 = vars[y_start + t - 1];
+        AD<double> psi0 = vars[psi_start + t - 1];
+        AD<double> v0 = vars[v_start + t - 1];
+        AD<double> cte0 = vars[cte_start + t - 1];
+        AD<double> epsi0 = vars[epsi_start + t - 1];
 
-      AD<double> a0 = vars[a_start + t - 1];
-      AD<double> delta0 = vars[delta_start + t - 1];
+        AD<double> a0 = vars[a_start + t - 1];
+        AD<double> delta0 = vars[delta_start + t - 1];
 
-      fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
-      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-      fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0/Lf * dt);
-      fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+        fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+        fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+        fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0/Lf * dt);
+        fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
 
-      //Target position from path fit.
-      AD<double> f0 = coeffs[3] * x0*x0*x0 + coeffs[2] * x0*x0 + coeffs[1] * x0 + coeffs[0];
-      fg[1 + cte_start + t] = cte1 - ((f0 - y0) + v0 * CppAD::sin(epsi0) * dt);
+        //Target position from path fit.
+        AD<double> f0 = coeffs[3] * x0*x0*x0 + coeffs[2] * x0*x0 + coeffs[1] * x0 + coeffs[0];
+        fg[1 + cte_start + t] = cte1 - ((f0 - y0) + v0 * CppAD::sin(epsi0) * dt);
 
-      //Target heading from path fit.
-      AD<double> df0 = 3*coeffs[3] * x0*x0 + 2*coeffs[2] * x0 + coeffs[1];
-      AD<double> psides0 = CppAD::atan(df0);
-      fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+        //Target heading from path fit.
+        AD<double> df0 = 3*coeffs[3] * x0*x0 + 2*coeffs[2] * x0 + coeffs[1];
+        AD<double> psides0 = CppAD::atan(df0);
+        fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+      }
     }
   }
 };
@@ -204,7 +205,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   // object that computes objective and constraints
   FG_eval fg_eval(coeffs);
-  
+
   //
   // NOTE: You don't have to worry about these options
   //
